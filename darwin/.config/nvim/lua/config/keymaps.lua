a9e1480007450f1c6817bccaa0082f0e240149d5
René Hansen <renehh@gmail.com>
2023-08-20 13:47:15 +0200

    Move lazygit bindings to vim-fugitive

--- darwin/.config/nvim/lua/config/keymaps.lua
+++ darwin/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap.set
+
+-- Unmap previous LazyGit mappings and add on for Vim fugitive
+keymap("n", "<leader>gg", "<cmd>vert Git<cr>", { desc = "Vim fugitive" })
+keymap("n", "<leader>gG", "")
