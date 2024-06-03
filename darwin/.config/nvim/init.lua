02074db69918e1801d22d01da29b51ae6db4ccfb
René Hansen <renehh@gmail.com>
2024-06-03 20:37:37 +0200

    Adds telescope fuzzy finder

--- darwin/.config/nvim/init.lua
+++ darwin/.config/nvim/init.lua
@@ -17,7 +17,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- lazy.vim setup
+-- lazy.nvim setup
 
 local plugins = {
   {
@@ -25,12 +25,24 @@ local plugins = {
     lazy = false,
     priority = 1000,
     opts = {},
+  },
+  {
+  'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' }
   }
 }
 
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+
+-- fuzzy file finding: telescope.nvim
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 -- colorscheme
 
