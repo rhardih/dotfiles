1c44185bd6c1220a48bddd8fe7bbf01ad6aee43a
René Hansen <renehh@gmail.com>
2024-06-09 22:04:33 +0200

    Adds none-ls

---
+++ darwin/.config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,16 @@
+-- linting etc. from cli tools as a language server
+
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
+}
