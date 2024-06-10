808b5f5cc359e1d154aa5e98b43750cb60b5eb83
René Hansen <renehh@gmail.com>
2024-06-10 21:48:09 +0200

    Adds trouble.nvim and lualine config

--- darwin/.config/nvim/lua/plugins/lualine.lua
+++ darwin/.config/nvim/lua/plugins/lualine.lua
@@ -1,11 +1,28 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'horizon'
-      }
+    local config = require("lualine").get_config()
+
+    config.options.theme = "horizon"
+
+    local trouble = require("trouble")
+    local symbols = trouble.statusline({
+      mode = "lsp_document_symbols",
+      groups = {},
+      title = false,
+      filter = { range = true },
+      format = "{kind_icon}{symbol.name:Normal}",
+      -- The following line is needed to fix the background color
+      -- Set it to the lualine section you want to use
+      hl_group = "lualine_c_normal",
     })
-  end
+
+    table.insert(config.sections.lualine_c, {
+      symbols.get,
+      cond = symbols.has,
+    })
+
+    require("lualine").setup(config)
+  end,
 }
