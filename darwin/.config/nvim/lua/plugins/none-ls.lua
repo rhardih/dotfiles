3af065afcb3d823b037c13a8481eac86b3849f42
René Hansen <renehh@gmail.com>
2024-06-11 11:46:46 +0200

    Adds goimports & gofumpt to none-ls

--- darwin/.config/nvim/lua/plugins/none-ls.lua
+++ darwin/.config/nvim/lua/plugins/none-ls.lua
@@ -8,6 +8,8 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.gofumpt,
       },
     })
 
