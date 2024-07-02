ddfc9c905749bb4f5d48877c1d3e37b8b151df0f
René Hansen <renehh@gmail.com>
2024-07-02 17:21:57 +0200

    Adds Telescope binding for "find word"

--- darwin/.config/nvim/lua/plugins/telescope.lua
+++ darwin/.config/nvim/lua/plugins/telescope.lua
@@ -12,6 +12,7 @@ return {
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
       vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+      vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
     end
   },
   { -- nice ui that replaces the quickfix list for e.g. code actions
