a59ab215f95b0d6ecfc80d8f9153c6e73f92f22f
René Hansen <renehh@gmail.com>
2024-07-18 17:08:14 +0200

    Use tabs in .go files

--- darwin/.config/nvim/lua/options.lua
+++ darwin/.config/nvim/lua/options.lua
@@ -20,6 +20,16 @@ opt.softtabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 
+-- Use tabs for Go files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "go",
+	callback = function()
+		vim.opt_local.expandtab = false
+		vim.opt_local.tabstop = 2
+		vim.opt_local.shiftwidth = 2
+	end,
+})
+
 -- folding
 
 opt.foldmethod = "expr"
