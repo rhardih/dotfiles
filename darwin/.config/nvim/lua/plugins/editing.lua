c5dc3a21657bdd9d628cfb0ddd3b43abe752ee4b
René Hansen <renehh@gmail.com>
2024-07-25 22:22:02 +0200

    Adds 'substitute'

--- darwin/.config/nvim/lua/plugins/editing.lua
+++ darwin/.config/nvim/lua/plugins/editing.lua
@@ -1,11 +1,22 @@
 return {
-  {
-    -- makes it easy to split and join lines of code with gS and gJ
-    "AndrewRadev/splitjoin.vim",
-  },
-  {
-    -- work with surrounding characters easily, ysiw", to surround a word with
-    -- " e.g.
-    "tpope/vim-surround",
-  },
+	{
+		-- makes it easy to split and join lines of code with gS and gJ
+		"AndrewRadev/splitjoin.vim",
+	},
+	{
+		-- work with surrounding characters easily, ysiw", to surround a word with
+		-- " e.g.
+		"tpope/vim-surround",
+	},
+	{
+		"gbprod/substitute.nvim",
+		config = function()
+			require("substitute").setup()
+
+			vim.keymap.set("n", "s", require("substitute").operator, { noremap = true })
+			vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
+			vim.keymap.set("n", "S", require("substitute").eol, { noremap = true })
+			vim.keymap.set("x", "s", require("substitute").visual, { noremap = true })
+		end,
+	},
 }
